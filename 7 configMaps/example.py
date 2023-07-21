86031c599a8812fd9145285839fe8a3656da5ee7
Anurag <anurag.chiplunkar@gmail.com>
2023-07-21 14:41:47 +0530

    updated

--- 7 configMaps/example.py
+++ 7 configMaps/example.py
@@ -1,4 +1,5 @@
 import os
 
-config_value = os.environ.get("CONFIG_VALUE", "default_value")
-print("Config Value:", config_value)
+for i in range(100):
+    config_value = os.environ.get("CONFIG_VALUE", "default_value")
+    print("Config Value:", config_value)
